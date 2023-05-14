d929221d027579e197279611c4a0f6144f94d6b6
Br2ath <sumi2266@naver.com>
2023-05-15 08:11:49 +0900

    Connect to firebase and correct errors

--- app/src/main/java/com/example/mp23_termproject_voldemorp/LoginActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.mp23_termproject_voldemorp;
 
 import androidx.annotation.NonNull;
-
 import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,9 +19,11 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 // 파이어베이스 연동 관련 부분
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.Ktx.auth;
-//import com.google.firebase.ktx.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -30,7 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     Button loginBtn;
     EditText loginEmailEditText;
     EditText loginPasswordEditText;
-    //private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser user;
 
     // 화면 터치시 키보드 내리기
     @Override
@@ -57,8 +59,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // 파이어 베이스 인증 객체 선언 및 버튼 등록
-        //firebaseAuth = FirebaseAuth.getInstance();
-        loginBtn = (Button)findViewById(R.id.loginButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginBtn = (Button) findViewById(R.id.loginButton);
         loginEmailEditText = findViewById(R.id.editTextEmailAddress);
         loginPasswordEditText = findViewById(R.id.editTextPassword);
 
@@ -74,41 +76,44 @@ public class LoginActivity extends AppCompatActivity {
             signUpDialog.show();
         });
 
-        // 로그인 성공 -> 유저정보 넘겨주고 메인 화면 호출하는 함수
-        public void moveMainPage(FirebaseUser user) {
-            if (user != null) {
-                Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
-                startActivity(intent);
-
-                // 화면 전환 시 왼쪽에서 오른쪽으로 밀듯이 나타나는 애니메이션 적용
-                overridePendingTransition(R.anim.slide_right_enter, R.anim.none);
-
-                finish();
-            }
-        }
-
 
         //                       --- 로그인  ---
 
-        loginBtn.setOnClickListner(new View.OnClickListener(){
+        loginBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 String email = loginEmailEditText.getText().toString().trim();
                 String pwd = loginPasswordEditText.getText().toString().trim();
-                //firebaseAuth.signInWithEmailAndPassword(email, pwd)
-                // .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> tast) {
-                    // 로그인 성공시 메인화면으로 넘어감
-                    if(task.isSuccessful()){
-                        moveMainPage(FirebaseAuth.getInstance().getCurrentUser());
-                    }
-                    else{ // 로그인 실패시 토스트 메시지 출력
-                        Toast.makeText(LoginActivity.this, "이메일 또는 비밀번호를 잘못 입력했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // 로그인 성공시 메인화면으로 넘어감
+                                if (task.isSuccessful()) {
+                                    moveMainPage(FirebaseAuth.getInstance().getCurrentUser());
+                                } else { // 로그인 실패시 토스트 메시지 출력
+                                    Toast.makeText(LoginActivity.this, "이메일 또는 비밀번호를 잘못 입력했습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
+
         });
+    }
+
+
+
+    // 로그인 성공 -> 유저정보 넘겨주고 메인 화면 호출하는 함수
+    public void moveMainPage(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+            startActivity(intent);
+
+            // 화면 전환 시 왼쪽에서 오른쪽으로 밀듯이 나타나는 애니메이션 적용
+            overridePendingTransition(R.anim.slide_right_enter, R.anim.none);
+
+            finish();
+        }
     }
 }
