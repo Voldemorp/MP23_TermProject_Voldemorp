0b037fd59f8600531446d7be09becaf1b80f889c
youz2me <kynhun20@gachon.ac.kr>
2023-05-15 18:43:45 +0900

    FEAT: Update SetLocationActivity

--- app/src/main/java/com/example/mp23_termproject_voldemorp/SetLocationActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/SetLocationActivity.java
@@ -3,6 +3,8 @@ package com.example.mp23_termproject_voldemorp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,10 @@ public class SetLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
+
+        // 바텀 시트 화면에 뿌려줌
+        SetLocationBottomSheet setLocationBottomSheet = new SetLocationBottomSheet();
+        setLocationBottomSheet.show(getSupportFragmentManager(), "setLocationBottomSheet");
 
         // 로그인으로 돌아가기 버튼 눌렀을 때 이벤트 -> 로그인 화면으로 화면 전환
         Button backToLoginBtn = (Button) findViewById(R.id.setLocationBackToLoginBtn);
