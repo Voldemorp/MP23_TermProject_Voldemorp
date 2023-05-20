3ba53ddc7a9e7007a2705b6dd7b3eb8c871da97f
youz2me <kynhun20@gachon.ac.kr>
2023-05-20 13:42:08 +0900

    FEAT: Create MypageActivity

---
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/MyPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mp23_termproject_voldemorp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
