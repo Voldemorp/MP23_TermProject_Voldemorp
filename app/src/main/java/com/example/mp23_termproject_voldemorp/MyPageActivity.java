216d22a4f4163aa71c4ae37bda43e614dfcdfe96
youz2me <kynhun20@gachon.ac.kr>
2023-05-21 22:15:34 +0900

    FEAT: Update activity_my_page.xml

--- app/src/main/java/com/example/mp23_termproject_voldemorp/MyPageActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/MyPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.mp23_termproject_voldemorp;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,9 @@ public class MyPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        // 상태 바 투명하게 하고 사진 보이게 하는 코드
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
     }
 }
