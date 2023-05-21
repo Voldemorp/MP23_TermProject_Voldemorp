918335814ca6aaa350bb63ca992d247a6c42fc08
youz2me <kynhun20@gachon.ac.kr>
2023-05-22 02:34:50 +0900

    FEAT: Create EditProfileActivity

--- app/src/main/java/com/example/mp23_termproject_voldemorp/EditProfileActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/EditProfileActivity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_edit_profile);
     }
 }
