301eda4d5c1238f99509962cfdb64e9529edd79b
Br2ath <sumi2266@naver.com>
2023-05-26 01:24:18 +0900

    create bottom navigation

---
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/navigationMypage.java
@@ -0,0 +1,15 @@
+package com.example.mp23_termproject_voldemorp;
+
+// 하단 탭바 이동 설정
+public class navigationMypage extends BottomNavigation {
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_my_page;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.mypage;
+    }
+}
