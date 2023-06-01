5ccfa421bd249d2a4c24b6d53256ac07a6ccb3cd
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-06-02 05:14:32 +0900

    Update MainActivity and so on

--- app/src/main/java/com/example/mp23_termproject_voldemorp/MainRestaurantListLayout.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/MainRestaurantListLayout.java
@@ -1,13 +1,16 @@
 package com.example.mp23_termproject_voldemorp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainRestaurantListLayout extends LinearLayout {
+
     public MainRestaurantListLayout(Context context, AttributeSet attrs, MainRestaurantInfo mainRestaurantInfo) {
         super(context, attrs);
         init(context, mainRestaurantInfo);
@@ -31,5 +34,19 @@ public class MainRestaurantListLayout extends LinearLayout {
 
         // 유저가 이 곳을 추천했는지 여부에 따라 하트 채워지도록 조정
         // userVisited.setText(mainRestaurantInfo.isHeartFilled);
+
+        // Set OnClickListener for the list item
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch RestaurantActivity with selected restaurant's name and type
+                Intent intent = new Intent(context, RestaurantActivity.class);
+                intent.putExtra("name", mainRestaurantInfo.name);
+                intent.putExtra("type", mainRestaurantInfo.foodType);
+                intent.putExtra("res_lat", mainRestaurantInfo.x);
+                intent.putExtra("res_long", mainRestaurantInfo.y);
+                context.startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mp23_termproject_voldemorp/RankingActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/RankingActivity.java
@@ -2,6 +2,7 @@ package com.example.mp23_termproject_voldemorp;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,9 @@ public class RankingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        // 상태 바 투명하게 하고 사진 보이게 하는 코드
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         rankingContainer = findViewById(R.id.rankingContainer);
         viewPager = new ViewPager2(this);
