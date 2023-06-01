163c1acbb49dceee37235aa15bd798e167a914f2
youz2me <kynhun20@gachon.ac.kr>
2023-06-02 02:21:16 +0900

    temp

--- app/src/main/java/com/example/mp23_termproject_voldemorp/MainActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
