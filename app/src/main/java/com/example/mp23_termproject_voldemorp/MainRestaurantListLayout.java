02be5449ce27ed7dd253f4b274a498678c081098
youz2me <kynhun20@gachon.ac.kr>
2023-06-03 02:20:33 +0900

    FIX: fix error

--- app/src/main/java/com/example/mp23_termproject_voldemorp/MainRestaurantListLayout.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/MainRestaurantListLayout.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
