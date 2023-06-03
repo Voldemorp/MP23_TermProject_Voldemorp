52a385b44204d44f4261c3ed3d758ab69bb55a65
Br2ath <sumi2266@naver.com>
2023-06-03 12:11:31 +0900

    FEAT:Create recommendation popup

--- app/src/main/java/com/example/mp23_termproject_voldemorp/RestaurantActivity.java
+++ app/src/main/java/com/example/mp23_termproject_voldemorp/RestaurantActivity.java
@@ -1,8 +1,18 @@
 package com.example.mp23_termproject_voldemorp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +20,83 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RestaurantActivity extends AppCompatActivity {
 
+    //현재사용자위치
+    static double latitude;
+    static double longitude;
+
+    //식당위치
+    private double res_lat;
+    private double res_long;
     private ViewPager2 viewPager;
     private FrameLayout restaurantContainer;
+
+    private Button portButton;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        // 상태 바 투명하게 하고 사진 보이게 하는 코드
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // MainActivity에서 전달된 인텐트 가져오기
+        Intent intent = getIntent();
+        String restaurantName = intent.getStringExtra("name");
+        String restaurantType = intent.getStringExtra("type");
+        res_lat = intent.getDoubleExtra("res_lat", 0.0);
+        res_long = intent.getDoubleExtra("res_long", 0.0);
+
+        //식당 이름
+        TextView resNameText = (TextView) findViewById(R.id.textView2);
+        //음식 타입
+        TextView foodTypeText = (TextView) findViewById(R.id.textView3);
+
+        resNameText.setText(restaurantName);
+        foodTypeText.setText(restaurantType);
+
+        // 현재 내 위치 인텐트에서 위도(latitude)와 경도(longitude) 값 추출
+        latitude = intent.getDoubleExtra("latitude", 0.0);
+        longitude = intent.getDoubleExtra("longitude", 0.0);
+
+        portButton = findViewById(R.id.portButton);
+
+        portButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // [서버] 포트버튼 클릭시 방문수 +1 해서 DB저장
+                // 추천 팝업창 띄우기
+                showPopup();
+                Toast.makeText(getApplicationContext(), "port success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 현재 사용자 위치와 식당 위치 간의 거리 계산
+        Location userLocation = new Location("user");
+        userLocation.setLatitude(latitude);
+        userLocation.setLongitude(longitude);
+
+        Location restaurantLocation = new Location("restaurant");
+        restaurantLocation.setLatitude(res_lat);
+        restaurantLocation.setLongitude(res_long);
+
+        float distance = userLocation.distanceTo(restaurantLocation); // 거리 계산
+        Toast.makeText(getApplicationContext(), String.valueOf(distance), Toast.LENGTH_SHORT).show();
+        // 거리가 300m 이내인 경우 버튼 활성화, 그렇지 않으면 비활성화
+        if (distance <= 100) {
+            portButton.setEnabled(true);
+            //팝업창 띄워
+        } else {
+            portButton.setEnabled(false);
+            Toast.makeText(getApplicationContext(), "port not possible", Toast.LENGTH_SHORT).show();
+        }
 
         restaurantContainer = findViewById(R.id.restaurantContainer);
         viewPager = new ViewPager2(this);
@@ -57,5 +135,64 @@ public class RestaurantActivity extends AppCompatActivity {
                     return null;
             }
         }
+    }
+
+    //   *---추천 팝업창 ---*
+    private AlertDialog dialog;
+    private void showPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View popupView = getLayoutInflater().inflate(R.layout.dialog_recommend_alert, null);
+
+        // 팝업 레이아웃을 AlertDialog에 설정
+        builder.setView(popupView);
+        dialog = builder.create();
+        dialog.show();
+
+        TextView visitTextView = popupView.findViewById(R.id.numOfVisit);
+        TextView visitMessage = popupView.findViewById(R.id.visitMessage);
+        TextView doyourecommend = popupView.findViewById(R.id.doyourecommend);
+        ImageView badgeImage = popupView.findViewById(R.id.badgeImage);
+        Button notRecommendBtn = popupView.findViewById(R.id.notRecommendBtn);
+        Button recommendBtn = popupView.findViewById(R.id.recommendBtn);
+        Button closeBtn = popupView.findViewById(R.id.closeBtn);
+
+        // [서버] 유저의 방문수 불러와서 visitNum에 저장, 임의로 넣은 int visitnum은 빼주세용
+        // "몇" 번째 방문의 '방문수' 표시
+        int visitNum = 2;
+        String strvisitNum = String.valueOf(visitNum);
+        visitTextView.setText(strvisitNum);
+
+        // 첫 방문시 추천여부 버튼이 보이지 않음
+        if (visitNum == 1) {
+            recommendBtn.setVisibility(View.GONE); // 버튼을 숨김 처리
+            notRecommendBtn.setVisibility(View.GONE); // 버튼을 숨김 처리
+            doyourecommend.setVisibility(View.GONE); // 텍스트뷰를 숨김 처리
+        }
+
+        // 두번째 방문부터
+        if (visitNum >= 2) {
+            recommendBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // [서버] 추천함 누를 시 추천 T로 변경
+                    dialog.dismiss();
+                }
+            });
+            notRecommendBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // [서버] 추천안함 누를 시 추천 F로 변경
+                    dialog.dismiss();
+                }
+            });
+        }
+
+        // 닫힘 버튼 'X'
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
 }
